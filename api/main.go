b854380b87a77d0efe953f0b3797d76a2f0f3527
Naman Pancholi <mahendra.naman07@gmail.com>
2023-11-19 20:03:46 +0530

    initialising

---
+++ api/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"shorten-url/routes"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/:url", routes.ResolveURL)
+	app.Post("/api/v1", routes.ShortenURL)
+}
+
+func main() {
+	err := godotenv.Load()
+	app := fiber.New()
+}
