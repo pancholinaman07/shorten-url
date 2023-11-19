58b0e2a2a2abc0ccf7b332a29e3aae7a883ed60d
Naman Pancholi <mahendra.naman07@gmail.com>
2023-11-19 22:58:24 +0530

    created outline for shorten.go

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,1 +1,46 @@
 package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"shorten-url/helpers"
+	"time"
+)
+
+type request struct {
+	URL         string        `json:"url"`
+	CustomShort string        `json:"short"`
+	Expiry      time.Duration `json:"expiry"`
+}
+
+type response struct {
+	URL             string        `json:"url"`
+	CustomShort     string        `json:"short"`
+	Expiry          time.Duration `json:"expiry"`
+	XRateRemaining  int           `json:"rate_limit"`
+	XRateLimitReset time.Duration `json:"rate_limit_reset"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+
+	body := new(request)
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse JSON"})
+	}
+
+	// implement rate limiting
+
+	//check if the input if an actual URL
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"message": "invalid URL"})
+	}
+
+	// check for domain error
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "you cant connect with system :)"})
+	}
+
+	//enforce https, SSL
+
+	body.URL = helpers.EnforceHTTP(body.URL)
+}
