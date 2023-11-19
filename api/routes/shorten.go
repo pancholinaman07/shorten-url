4dc320c472e467219a10d2cf93af217020dfe85a
Naman Pancholi <mahendra.naman07@gmail.com>
2023-11-20 00:55:35 +0530

    completed shorten.go filea

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -95,5 +95,23 @@ func ShortenURL(c *fiber.Ctx) error {
 		})
 	}
 
+	resp := response{
+		URL:             body.URL,
+		CustomShort:     "",
+		Expiry:          body.Expiry,
+		XRateRemaining:  10,
+		XRateLimitReset: 30,
+	}
+
 	r2.Decr(database.Ctx, c.IP())
+
+	val, _ = r2.Get(database.Ctx, c.IP()).Result()
+	resp.XRateRemaining, _ = strconv.Atoi(val)
+
+	ttl, _ := r2.TTL(database.Ctx, c.IP()).Result()
+	resp.XRateLimitReset = ttl / time.Nanosecond / time.Minute
+
+	resp.CustomShort = os.Getenv("DOMAIN") + "/" + id
+
+	return c.Status(fiber.StatusOK).JSON(resp)
 }
