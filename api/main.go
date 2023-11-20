4930b571b2d7efcc982eba6a0e3ba86fdee9d344
Naman Pancholi <mahendra.naman07@gmail.com>
2023-11-20 10:23:26 +0530

    completed

--- api/main.go
+++ api/main.go
@@ -10,6 +10,11 @@ import (
 	"shorten-url/routes"
 )
 
+// setup two routes, one for shortening the url
+// the other for resolving the url
+// for example if the short is `4fg`, the user
+// must navigate to `localhost:3000/4fg` to redirect to
+// original URL. The domain can be changes in .env file
 func setupRoutes(app *fiber.App) {
 	app.Get("/:url", routes.ResolveURL)
 	app.Post("/api/v1", routes.ShortenURL)
@@ -17,13 +22,12 @@ func setupRoutes(app *fiber.App) {
 
 func main() {
 	err := godotenv.Load()
-
 	if err != nil {
 		fmt.Println(err)
 	}
-
 	app := fiber.New()
 
+	//app.Use(csrf.New())
 	app.Use(logger.New())
 
 	setupRoutes(app)
--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -6,30 +6,30 @@ import (
 	"shorten-url/database"
 )
 
+// ResolveURL ...
 func ResolveURL(c *fiber.Ctx) error {
-
+	// get the short from the url
 	url := c.Params("url")
-
+	// query the db to find the original URL, if a match is found
+	// increment the redirect counter and redirect to the original URL
+	// else return error message
 	r := database.CreateClient(0)
 	defer r.Close()
 
 	value, err := r.Get(database.Ctx, url).Result()
-	if err != redis.Nil {
+	if err == redis.Nil {
 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"error": "short not found",
+			"error": "short not found on database",
 		})
-	}
-	if err != nil {
+	} else if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"error": "cannot connect to DB",
 		})
 	}
-
+	// increment the counter
 	rInr := database.CreateClient(1)
 	defer rInr.Close()
-
 	_ = rInr.Incr(database.Ctx, "counter")
-
+	// redirect to original URL
 	return c.Redirect(value, 301)
-
 }
